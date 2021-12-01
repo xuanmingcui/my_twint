f1c8a32825502302f8538e86e56ea920397dbf7a
Sammy Cui <xcui16@stlawu.edu>
2021-12-01 16:07:26 -0500

    add tweets_daily_by_keyword func
    Add waiting time for guest token html not found error

--- my_twint/twint_wrapper.py
+++ my_twint/twint_wrapper.py
@@ -1,14 +1,17 @@
+import time
+
 from my_twint import twintv2 as twint
 import pandas as pd
 import numpy as np
 from typing import Union, List
+import datetime
+
+from my_twint.twintv2.token import RefreshTokenException
 
 
-def tweets_by_keyword(keyword: str, start: str = None, end: str = None, limit: int = 100, to_csv: str = None, lang='en',
-                      to_json: str = None, max_iter: int = 10, cols: List[str] = None) -> pd.DataFrame:
-    if cols is None:
-        cols = ['created_at', 'date', 'place', 'username', 'name', 'tweet', 'nlikes',
-                'nreplies', 'nretweets']
+def tweets_by_keyword(keyword: str, start: str = None, end: str = None, limit: int = 100, lang='en', to_csv: str = None,
+                      to_json: str = None, max_iter: int = 10, cols: List[str] = None,
+                      hide_output: bool = True) -> pd.DataFrame:
     result = pd.DataFrame()
     t = twint.Config()
     t.Search = keyword
@@ -18,18 +21,56 @@ def tweets_by_keyword(keyword: str, start: str = None, end: str = None, limit: i
     t.limit = limit
     t.Lang = lang
     failed_counter = 0
-    cols = cols if cols else ['created_at', 'date', 'place', 'username', 'name', 'tweet', 'nlikes',
+    t.Hide_output = hide_output
+    cols = cols if cols else ['date', 'place', 'username', 'name', 'tweet', 'nlikes',
                               'nreplies', 'nretweets']
     while len(result) < limit:
         try:
             twint.run.Search(t)
             df = twint.storage.panda.Tweets_df
-            result = result.append(df[cols])
-        except:
+            if len(df) > 0:
+                result = result.append(df[cols])
+            failed_counter = 0
+        except Exception as e:
+            # if any error happens, rerun search; if rerun iterations > max_iter, break
             failed_counter += 1
             if failed_counter >= max_iter:
-                raise ValueError(f"Repeated {max_iter} times, no result is found. "
-                                 f"Try upgrade twintv2 version or different IP address")
+                raise e
+    if to_csv:
+        result.to_csv(path_or_buf=to_csv, index=True)
+    if to_json:
+        result.to_json(path_or_buf=to_json, orient='records')
+    return result
+
+
+def tweets_daily_by_keyword(keyword: str, start: str = None, end: str = None, daily_limit: int = 100, lang='en',
+                            to_csv: str = None,
+                            to_json: str = None, max_iter: int = 10, cols: List[str] = None,
+                            hide_output: bool = True) -> pd.DataFrame:
+    cols = cols if cols else ['date', 'place', 'username', 'name', 'tweet', 'nlikes', 'nreplies', 'nretweets']
+
+    start_datetime, end_datetime = datetime.datetime.strptime(start, '%Y-%m-%d'), datetime.datetime.strptime(end,
+                                                                                                             '%Y-%m-%d')
+    result = pd.DataFrame(columns=cols)
+    failed_counter = 0
+    while start_datetime < end_datetime:
+        print(f'Fetching result between {str(start_datetime)} and '
+              f'{str(str(start_datetime + datetime.timedelta(days=1)))}')
+        try:
+            result = result.append(tweets_by_keyword(keyword=keyword, start=start_datetime.strftime('%Y-%m-%d'),
+                                                     end=(start_datetime + datetime.timedelta(days=1)).strftime(
+                                                         '%Y-%m-%d'),
+                                                     limit=daily_limit, lang=lang, to_csv=None, to_json=None,
+                                                     max_iter=max_iter, cols=cols, hide_output=hide_output))
+            start_datetime += datetime.timedelta(days=1)
+            failed_counter = 0
+        except RefreshTokenException as e:
+            failed_counter += 1
+            if failed_counter > 3:
+                raise e
+            time.sleep(1500)
+            pass
+
     if to_csv:
         result.to_csv(path_or_buf=to_csv, index=True)
     if to_json:
@@ -38,4 +79,6 @@ def tweets_by_keyword(keyword: str, start: str = None, end: str = None, limit: i
 
 
 if __name__ == '__main__':
-    print(tweets_by_keyword('iphone', '2020-01-01', '2020-01-02', limit=100))
+    df = tweets_daily_by_keyword('tsla OR TSLA or Tesla', '2018-01-01', '2021-11-01', daily_limit=100,
+                                 to_csv='tsla_tweets.csv')
+    print(len(df))
